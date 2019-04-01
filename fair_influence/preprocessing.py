252511903e7e2f11578d397077ecac729eaf8cf0
Dany Haddad <danyhaddad43@gmail.com>
2019-04-01 06:48:28 -0500

    add saf details

--- fair_influence/preprocessing.py
+++ fair_influence/preprocessing.py
@@ -22,3 +22,33 @@ def prepare_adult(df: pd.DataFrame,
                                     is_primary[:, np.newaxis]],
                                    1))
   return data, y
+
+def convert_bin(col):
+  if len(col) == 0: return col
+  default = col[0]
+  return np.float32([elem == default for elem in col])
+
+def prepare_saf(df: pd.DataFrame,
+                target='frisked',
+                protected='race',
+                primary='W') -> np.ndarray:
+  assert target in ['frisked', 'searched']
+  col_names = ['inout', 'recstat','pct', 'ser_num', 'timestop', 'trhsloc', 'perobs', 'crimsusp', 'typeofid', 'explnstp', 'othpers', 'arstmade', 'arstoffn', 'sumissue', 'sumoffen', 'offunif', 'officrid', 'contrabn', 'adtlrept', 'pistol', 'riflshot', 'asltweap', 'knifcuti', 'machgun', 'othrweap', 'pf_hands', 'pf_wall', 'pf_grnd', 'pf_drwep', 'pf_ptwep', 'pf_baton', 'pf_hcuff', 'pf_pepsp', 'pf_other', 'radio', 'ac_rept', 'ac_inves', 'rf_vcrim', 'rf_othsw', 'ac_proxm', 'rf_attir', 'cs_objcs', 'cs_descr', 'cs_casng', 'cs_lkout', 'rf_vcact', 'cs_cloth', 'cs_drgtr', 'ac_evasv', 'ac_assoc', 'cs_furtv', 'rf_rfcmp', 'ac_cgdir', 'rf_verbl', 'cs_vcrim', 'cs_bulge', 'cs_other', 'ac_incid', 'ac_time', 'rf_knowl', 'ac_stsnd', 'ac_other', 'sb_hdobj', 'sb_outln', 'sb_admis', 'sb_other', 'repcmd', 'revcmd', 'rf_furt', 'rf_bulg', 'offverb', 'offshld', 'forceuse', 'sex', 'race', 'age', 'ht_feet', 'weight', 'haircolr', 'eyecolor', 'build', 'city', 'addrpct', 'sector']
+  bin_names = ['contrabn','adtlrept','pistol','riflshot','asltweap','knifcuti','machgun','othrweap','pf_hands','pf_wall','pf_grnd','pf_drwep','pf_ptwep','pf_baton','pf_hcuff','pf_pepsp','pf_other','radio','ac_rept','ac_inves','rf_vcrim','rf_othsw','ac_proxm','rf_attir','cs_objcs','cs_descr','cs_casng','cs_lkout','rf_vcact','cs_cloth','cs_drgtr','ac_evasv','ac_assoc','cs_furtv','rf_rfcmp','ac_cgdir','rf_verbl','cs_vcrim','cs_bulge','cs_other','ac_incid','ac_time','rf_knowl','ac_stsnd','ac_other','sb_hdobj','sb_outln','sb_admis','sb_other','offunif','sumissue','othpers','arstmade','explnstp','rf_furt','rf_bulg']
+  cat_names = ['inout', 'recstat','arstoffn','officrid','trhsloc','crimsusp','typeofid','sumoffen','offverb','offshld','forceuse','sex','race','age','ht_feet','weight','haircolr','eyecolor','build','city','addrpct','sector']
+  not_cat_names = list(set(col_names) - set(cat_names) - set(bin_names))
+  except_protected_names = [name for name in cat_names if name != protected]
+  not_cat = df[not_cat_names]
+  bin_cols = np.vstack([convert_bin(df[col_name]) for col_name in bin_names]).T
+  with_dummies = pd.get_dummies(df[except_protected_names], columns=except_protected_names).values
+  is_primary = np.float32((df[protected] == primary).values)
+  y = np.float32((df[target].str.contains('Y')).values)
+  centered_not_cat = []
+  for name in not_cat_names:
+    centered_not_cat.append((not_cat[name] - not_cat[name].mean()) / not_cat[name].std())
+  data = np.float32(np.concatenate([np.array(centered_not_cat).T,
+                                    bin_cols,
+                                    with_dummies,
+                                    is_primary[:, np.newaxis]],
+                                   1))
+  return data, y
--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import seaborn as sns
 
 import pydash as _
 
-from fair_influence.fetchers import get_train_test_adult
-from fair_influence.preprocessing import prepare_adult
+from fair_influence.fetchers import get_train_test_adult, get_train_test_saf
+from fair_influence.preprocessing import prepare_adult, prepare_saf
 from fair_influence.logistic_regression import TorchLogisticRegression
 from fair_influence.fairness import calc_fairness, calc_pred_fairness, make_more_fair_retrain, eval_fairness
 from fair_influence.trainer import Trainer
@@ -20,7 +20,7 @@ from fair_influence.inference import eval_acc
 from fair_influence.influence import calc_log_reg_hvp_inverse, calc_log_reg_grad, calc_s_tests, calc_influences, calc_log_reg_dkl_grad
 from fair_influence.explore import scatter_dists, get_fisher_vectors
 
-def main():
+def adult_analysis():
   raw_train, raw_test = get_train_test_adult()
   raw_train = raw_train[['age', 'fnlwgt', 'education-num', 'race', 'capital-gain', 'capital-loss', 'hours-per-week', 'sex', 'target']]
   raw_test = raw_test[['age', 'fnlwgt', 'education-num', 'race', 'capital-gain', 'capital-loss', 'hours-per-week', 'sex', 'target']]
@@ -82,5 +82,41 @@ def main():
   remain_y = y_train[list(set(range(len(y_train))) - set(idxs_to_drop.tolist()))]
   orig_corrs = [np.corrcoef(X_train[:, i], y_train)[0, 1] for i in range(X_train.shape[1])]
   remain_corrs = [np.corrcoef(remain_X[:, i], remain_y)[0, 1] for i in range(remain_X.shape[1])]
+
+def saf_analysis():
+  raw_train, raw_test = get_train_test_saf()
+  X, y = [torch.tensor(arr) for arr in prepare_saf(pd.concat([raw_train, raw_test]))]
+  X_train = X[:len(raw_train)]
+  y_train = y[:len(raw_train)]
+  X_test  = X[len(raw_train):len(raw_train) + len(raw_test)]
+  y_test  = y[len(raw_train):len(raw_train) + len(raw_test)]
+  get_model = lambda: TorchLogisticRegression(X_train.shape[1])
+  trainer = Trainer(get_model, nn.BCELoss())
+  trainer.train(X_train, y_train, batch_size=1000, num_epochs=100, reg=0.0, verbose=False)
+  unfair_model = trainer.model
+  print('acc:', eval_acc(unfair_model, X_test, y_test))
+  print('train: KL p(y | female), p(y | male)', calc_fairness(X_train, y_train))
+  print('test: KL p(y | female), p(y | male)', calc_fairness(X_test, y_test))
+  print('model: KL p(y | female), p(y | male)', eval_fairness(unfair_model, X_test))
+  calc_hvp_inv = lambda model, grads, reg: calc_log_reg_hvp_inverse(model, X_train, grads, reg=reg)
+  calc_grad = lambda model, data, target: calc_log_reg_grad(model, data, target)
+  calc_dkl_grad = lambda model, data, target: calc_log_reg_dkl_grad(model, data, target)
+  s_tests = calc_s_tests(unfair_model, calc_hvp_inv, calc_dkl_grad, X_test, y_test, reg=0.06)
+  influences = calc_influences(unfair_model, calc_grad, s_tests, X_train, y_train).squeeze()
+  idxs_to_drop = (influences > 0).nonzero()[:, 0]
+  trainer.retrain_leave_one_out(X_train, y_train, idxs_to_drop, reg=0.0, batch_size=1000, num_epochs=100, verbose=False)
+  fair_model = trainer.model
+  print('model retrain: KL p(y | female), p(y | male)', eval_fairness(fair_model, X_test))
+  print('acc:', eval_acc(fair_model, X_test, y_test))
+  female = ((X_train[:, -1].byte()).int() & y_train.int()).nonzero().squeeze()
+  trainer.retrain_leave_one_out(X_train, y_train, female[:len(idxs_to_drop)], reg=0.0, batch_size=1000, num_epochs=100, verbose=False)
+  female_model = trainer.model
+  print('female model retrain: KL p(y | female), p(y | male)', eval_fairness(female_model, X_test))
+  print('acc:', eval_acc(female_model, X_test, y_test))
+
+
+def main():
+  # adult_analysis()
+  saf_analysis()
 
 if __name__ == "__main__": main()
